ccec676f8070904b46aeaa8289ccaf7e9af7c02c
HVT11 <huynhtri11102000@gmail.com>
2022-07-01 19:19:58 +0700

    Examples for union types, interfaces, literal types

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -27,4 +27,60 @@ const numberToString = (number: number):string => {
 }
 
 console.log(typeof numberToString(3))
-//
+
+//Object Types
+function printName(obj: { first: string; last?: string }) {
+    // ...
+}
+  
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+
+//Union Type
+function printId(id: number | string) {
+    if (typeof id === "string") {
+      // In this branch, id is of type 'string'
+      console.log(id.toUpperCase());
+    } else {
+      // Here, id is of type 'number'
+      console.log(id);
+    }
+}
+
+printId(101)
+printId("202")
+
+//Interfaces
+interface Point {
+    x: number,
+    y: number
+}
+
+function printCoord(pt: Point) {
+    console.log("The coordinate's x value is " + pt.x)
+    console.log("The coordinate's y value is " + pt.y)
+}
+
+printCoord({x: 100, y: 100})
+
+//Literal Types
+let changingString = "Hello World"
+changingString = "Olá Mundo"
+console.log(changingString)
+
+const req = { url: "https://example.com", method: "GET" } as const;
+
+//Enums
+enum UserResponse {
+    No,
+    Yes,
+  }
+   
+function respond(recipient: string, message: UserResponse) {
+    console.log(recipient, message)
+}
+   
+respond("Princess Caroline", UserResponse.No);
+
+
+
