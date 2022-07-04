52dfa78ed9fd549a603c22b4c95a3b22138a948a
HVT11 <huynhtri11102000@gmail.com>
2022-07-04 18:55:00 +0700

    Examples for enums

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -71,6 +71,7 @@ console.log(changingString)
 const req = { url: "https://example.com", method: "GET" } as const;
 
 //Enums
+//Numeric enums
 enum UserResponse {
     No,
     Yes,
@@ -82,5 +83,58 @@ function respond(recipient: string, message: UserResponse) {
    
 respond("Princess Caroline", UserResponse.No);
 
+//String Enums
+enum Direction {
+    Up = "UP",
+    Down = "DOWN",
+    Left = "LEFT",
+    Right = "RIGHT",
+}
 
+//Constant and computed members
+enum FileAccess {
+    // constant members
+    None,
+    Read = 1 << 1,
+    Write = 1 << 2,
+    ReadWrite = Read | Write,
+    // computed member
+    G = "123".length,
+}
 
+// Enum at compile time
+enum LogLevel {
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG,
+  }
+   
+/**
+ * This is equivalent to:
+ * type LogLevelStrings = 'ERROR' | 'WARN' | 'INFO' | 'DEBUG';
+ */
+type LogLevelStrings = keyof typeof LogLevel
+   
+function printImportant(key: LogLevelStrings, message: string) {
+    const num = LogLevel[key]
+    if (num <= LogLevel.WARN) {
+      console.log("Log level key is:", key)
+      console.log("Log level value is:", num)
+      console.log("Log level message is:", message)
+    }
+}
+printImportant("ERROR", "This is a message")
+
+// Class
+class Point1 {
+    readonly text: 'point'
+    x: number
+    y: number
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const pt = new Point1(0, 1);
